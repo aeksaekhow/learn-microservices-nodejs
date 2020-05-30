4766761fc3c2813356a3f71a68f574e93d5426ea
Aek Sae-khow <saekhow.aek@gmail.com>
2020-05-30 14:09:15 +0700

    157. Adding Session Support

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -7,9 +7,15 @@ import { signupRouter } from './routes/signup'
 import { errorHandler } from './middlewares/error-handler'
 import { NotFoundError } from './errors/not-found-error'
 import mongoose from 'mongoose'
+import cookieSession from 'cookie-session'
 
 const app = express()
+app.set('trust proxy', true) // make Express trust proxy from nginx
 app.use(json())
+app.use(cookieSession({
+    signed: false,
+    secure: true
+}))
 
 app.use(currentUserRouter)
 app.use(signInRouter)
